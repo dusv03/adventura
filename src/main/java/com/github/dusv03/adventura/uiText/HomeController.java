29a1d476b283c413875dc74c6b9d56f3f4143e6e
dusv03 <dusv03@sb105h03.ad.vse.cz>
2018-03-15 15:57:32 +0100

    asd

--- src/main/java/com/github/dusv03/adventura/uiText/HomeController.java
+++ src/main/java/com/github/dusv03/adventura/uiText/HomeController.java
@@ -1,8 +1,14 @@
 package com.github.dusv03.adventura.uiText;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.util.Observable;
+
 import com.github.dusv03.adventura.logika.IHra;
+import com.github.dusv03.adventura.logika.Prostor;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
@@ -10,13 +16,34 @@ import javafx.scene.layout.GridPane;
 public class HomeController extends GridPane{
 	
 	@FXML private TextField textVstup;
+	@FXML private TextArea textVypis;
+	@FXML private Button odesli;
+	@FXML private ListView<Prostor> seznamMistnosti;
+	private IHra hra;
 
 	public void odesli() {
-		System.out.println(textVstup.getText());
+		String vypis = hra.zpracujPrikaz(textVstup.getText());
+		textVypis.appendText("\n----------\n"+textVstup.getText()+"\n-------\n");
+		textVypis.appendText(vypis);
+		textVstup.setText("");
+		
+		if (hra.konecHry()){
+			textVypis.appendText("\n\n Konec hry \n");
+			textVstup.setDisable(true);
+			odesli.setDisable(true);
+			
+		}
 	}
 	
 	public void inicializuj(IHra hra){
-		
+		this.hra = hra;
+		textVypis.setText(hra.vratUvitani());
+		seznamMistnosti.getItems().addAll(hra.getHerniPlan().getAktualniProstor().getVychody());
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
+	
 	}
 	
 }
